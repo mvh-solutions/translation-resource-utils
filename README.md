928ff097622b4a4cdec50d8a0aef5ad42917c88c
Mark Howe <mvahowe@gmail.com>
2022-07-11 09:09:37 +0200

    deepL tn9 translation in Python

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# translation-resource-utils
+# Translation Resource Utils
 Utilities for working with Unfolding Word translation resources
+
+## Credentials
+The `translate_tn9.py` script requires a deepL API key, provided via a JSON file:
+```
+{
+    "deepLAuthKey": "my-deepl-key"
+}
+```
+See https://www.deepl.com/docs-api
+
+## Usage
+
+```
+cd scripts
+python3.8 translate_tn9.py FR ../creds.json input/en_tn-3JN.tsv output/fr_tn-3JN.tsv
+```
---
+++ scripts/translate_tn9.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import sys
+import os
+
+usage = "USAGE: python translate_tn9.py <lang> <credsPath> <inPath> <outPath>"
+if len(sys.argv) != 5:
+    raise Exception(usage)
+lang = sys.argv[1]
+credsPath = sys.argv[2]
+if not(os.path.exists(credsPath)):
+    raise Exception("Path to credentials {0} does not exist\n{1}".format(credsPath, usage))
+inPath = sys.argv[3]
+if not(os.path.exists(inPath)):
+    raise Exception("Input path {0} does not exist\n{1}".format(inPath, usage))
+outPath = sys.argv[4]
+if os.path.exists(outPath):
+    raise Exception("Output path {0} already exists\n{1}".format(outPath, usage))
+
+with open(os.path.abspath(credsPath), 'r') as f:
+    credsString = ''.join(f.readlines())
+    auth_key = json.loads(credsString)['deepLAuthKey']
+
+url = 'https://api.deepl.com/v2/translate'
+
+outputLines = []
+with open(os.path.abspath(inPath), 'r') as f:
+    lineNo = 0
+    for line in f.readlines():
+        cells = line.split('\t')
+        if lineNo > 0:
+            srcText = cells[8]
+            args = {
+                'auth_key': auth_key,
+                'text': srcText,
+                'target_lang': lang
+            }
+            response = requests.post(url, data=args)
+            cells[8] = json.loads(response.text)['translations'][0]['text']
+        outputLines.append('\t'.join(cells))
+        lineNo += 1
+
+with open(os.path.abspath(outPath), "w") as f:
+    f.write('\n'.join(outputLines))
